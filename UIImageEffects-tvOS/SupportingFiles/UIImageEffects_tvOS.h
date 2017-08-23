1d15f90efe67d4404dd55beb09b66adb3925211a
Seremet Mihai <mihai.seremet@ellation.com>
2017-08-23 23:00:54 +0300

    Add implementation

---
+++ UIImageEffects-tvOS/SupportingFiles/UIImageEffects_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  UIImageEffects_tvOS.h
+//  UIImageEffects-tvOS
+//
+//  Created by Mihai Seremet on 8/23/17.
+//  Copyright © 2017 Mihai Seremet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageEffects_tvOS.
+FOUNDATION_EXPORT double UIImageEffects_tvOSVersionNumber;
+
+//! Project version string for UIImageEffects_tvOS.
+FOUNDATION_EXPORT const unsigned char UIImageEffects_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageEffects_tvOS/PublicHeader.h>
+
+
---
+++ UIImageEffects/SupportingFiles/UIImageEffects.h
@@ -0,0 +1,19 @@
+//
+//  UIImageEffects.h
+//  UIImageEffects
+//
+//  Created by Mihai Seremet on 8/23/17.
+//  Copyright © 2017 Mihai Seremet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageEffects.
+FOUNDATION_EXPORT double UIImageEffectsVersionNumber;
+
+//! Project version string for UIImageEffects.
+FOUNDATION_EXPORT const unsigned char UIImageEffectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageEffects/PublicHeader.h>
+
+
